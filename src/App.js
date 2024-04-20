70e746628e1be2c9b5af6462eb3186b0503f517c
kimhyunjumg <guswjddudwo@gmail.com>
2024-04-21 02:10:39 +0900

    header 생성중

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from "styled-components";
+import { Route, Routes } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css"; // bootstrap CSS 추가
+import "react-toastify/dist/ReactToastify.min.css"; // ReactToastify CSS 추가
+
+const GlobalStyle = createGlobalStyle`
+/* 글로벌(공통) 스타일 */
+body {
+    box-sizing: border-box;
+  }
+
+  #root {
+    text-align: center;
+  }
+
+  * {
+    box-sizing: inherit;
+  }
+
+  .cursor-pointer {
+    cursor: pointer;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+    </>
   );
 }
 
